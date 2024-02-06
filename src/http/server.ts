3517ecffa08c556487977a68fe7bdfda10612e89
Wilson Franca <wilson.franca.92@gmail.com>
2024-02-05 18:11:58 -0800

    feat(server): add first route to create a poll for testing

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,25 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import z from 'zod'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello'
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  })
+
+  const { title } = createPollBody.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  })
+
+  return reply.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
