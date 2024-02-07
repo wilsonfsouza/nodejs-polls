357526e1b223860c91ab23e3a3fb28ff69a63962
Wilson Franca <wilson.franca.92@gmail.com>
2024-02-07 06:16:55 -0800

    feat(create-polls): create options for each poll

--- src/http/controllers/create-poll.ts
+++ src/http/controllers/create-poll.ts
@@ -5,13 +5,21 @@ import z from 'zod'
 export async function createPoll(request: FastifyRequest, reply: FastifyReply) {
   const createPollBodySchema = z.object({
     title: z.string(),
+    options: z.array(z.string()),
   })
 
-  const { title } = createPollBodySchema.parse(request.body)
+  const { title, options } = createPollBodySchema.parse(request.body)
 
   const poll = await prisma.poll.create({
     data: {
       title,
+      options: {
+        createMany: {
+          data: options.map((option) => {
+            return { title: option }
+          }),
+        },
+      },
     },
   })
 
