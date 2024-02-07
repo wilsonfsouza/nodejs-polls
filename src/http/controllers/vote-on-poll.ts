4efe989529041f560342bc45ff4b095211cb4826
Wilson Franca <wilson.franca.92@gmail.com>
2024-02-07 09:49:23 -0800

    feat(vote-on-poll): add route to compute vote on a poll

---
+++ src/http/controllers/vote-on-poll.ts
@@ -0,0 +1,71 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import z from 'zod'
+
+export async function voteOnPoll(request: FastifyRequest, reply: FastifyReply) {
+  const voteOnPollSchema = z.object({
+    pollId: z.string().uuid(),
+  })
+
+  const voteOnPollBodySchema = z.object({
+    pollOptionId: z.string().uuid(),
+  })
+
+  const { pollId } = voteOnPollSchema.parse(request.params)
+  const { pollOptionId } = voteOnPollBodySchema.parse(request.body)
+
+  let { sessionId } = request.cookies
+
+  if (sessionId) {
+    const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+      where: {
+        sessionId_pollId: {
+          pollId,
+          sessionId,
+        },
+      },
+    })
+
+    if (
+      userPreviousVoteOnPoll &&
+      userPreviousVoteOnPoll.pollOptionId === pollOptionId
+    ) {
+      return reply
+        .status(400)
+        .send({ message: 'You already voted on this poll.' })
+    }
+
+    if (
+      userPreviousVoteOnPoll &&
+      userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+    ) {
+      await prisma.vote.delete({
+        where: {
+          id: userPreviousVoteOnPoll.id,
+        },
+      })
+    }
+  }
+
+  if (!sessionId) {
+    sessionId = randomUUID()
+
+    reply.setCookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 30, // 30 days
+      signed: true,
+      httpOnly: true,
+    })
+  }
+
+  await prisma.vote.create({
+    data: {
+      sessionId,
+      pollId,
+      pollOptionId,
+    },
+  })
+
+  return reply.status(201).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { createPoll } from './controllers/create-poll'
 import { getPoll } from './controllers/get-poll'
+import { voteOnPoll } from './controllers/vote-on-poll'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/polls', createPoll)
   app.get('/polls/:pollId', getPoll)
+  app.post('/polls/:pollId/votes', voteOnPoll)
 }
